2aae247fb179df296e1de8c6741a0c4a8a504925
samiam120 <samassefa3@gmail.com>
2025-01-22 17:57:28 -0800

    added eslint along with script for linting

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    rules:{
+      "no-undef": "warn",
+      "no-unused-vars": "warn"
+    }
+  }
+];
