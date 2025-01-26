fe7fd394b2a68825e755c0c94197ee6b42635fcf
samiam120 <samassefa3@gmail.com>
2025-01-25 20:56:02 -0800

    horizontal and vertical ship placement testing

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,6 +1,26 @@
 import Gameboard from "../src/gameboard"
+
+const board = new Gameboard();
+
 describe("Gameboard", () => {
-    test("", () => {
-        
-    })
-})
+    test("placeShip horizontally", () => {
+        const board = new Gameboard();
+        board.placeShip("carrier", { x: 0, y: 0 });
+        const gameBoard = board.getBoard();
+        for (let i = 0; i < 5; i++) {
+            expect(gameBoard[0][i]).toBe("carrier");
+        }
+    });
+
+    test("placeShip vertically", () => {
+        const board = new Gameboard();
+        board.ships.carrier.changeOrientation(); // Change to vertical
+        board.placeShip("carrier", { x: 0, y: 0 });
+        const gameBoard = board.getBoard();
+        for (let i = 0; i < 5; i++) {
+            expect(gameBoard[i][0]).toBe("carrier");
+        }
+    });
+});
+
+
