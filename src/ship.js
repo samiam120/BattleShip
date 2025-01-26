af0982dc4400a2f04ccdd2e09705ec99b1afa7f9
samiam120 <samassefa3@gmail.com>
2025-01-25 20:35:55 -0800

    horizontal boolean

--- src/ship.js
+++ src/ship.js
@@ -1,9 +1,9 @@
 class Ship {
-  
   constructor(name, length) {
     this.name = name;
     this._length = length;
     this.health = length;
+    this.horizontal = true; 
   }
 
   hit() {
@@ -14,6 +14,14 @@ class Ship {
     this.health--;
   }
 
+  changeOrientation() {
+    this.horizontal = !this.horizontal;
+  }
+
+  getOrientation() {
+    return this.horizontal;
+  }
+
   hitCount() {
     return this.health;
   }
@@ -22,15 +30,17 @@ class Ship {
     return this.health <= 0;
   }
 
-  length(){
+  length() {
     return this._length;
   }
 }
 
-export const ships = {
-  "carrier": new Ship("carrier", 5),
-  "battleship": new Ship("battleship", 4),
-  "cruiser": new Ship("cruiser", 3),
-  "submarine": new Ship("submarine", 3),
-  "destroyer": new Ship("destroyer", 2),
+export const ships = () => {
+  return {
+    carrier: new Ship("carrier", 5),
+    battleship: new Ship("battleship", 4),
+    cruiser: new Ship("cruiser", 3),
+    submarine: new Ship("submarine", 3),
+    destroyer: new Ship("destroyer", 2),
+  };
 };
